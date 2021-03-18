9d9f05b2f764f82fb177c2edb4b2e067d2090571
mf-2021 <manamu.fujiwara@gmail.com>
2021-03-18 15:55:10 +0000

    [Add]resource(Books_controller)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
+  get "home/about" => "homes#about", as: "about"
+  resources :books, only: [:new, :create, :index, :show, :destroy]
+
 end
