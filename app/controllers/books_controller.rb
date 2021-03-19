0fb3db2b02fe25a2f83dee704bb09bd5896b0e7d
mf-2021 <manamu.fujiwara@gmail.com>
2021-03-19 12:01:06 +0000

    [Add]Bootstrap

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,16 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
   end
 
   def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path
   end
 
   def index
+    @books = Book.all
   end
 
   def show
@@ -14,5 +19,11 @@ class BooksController < ApplicationController
   def destroy
   end
 
+
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_18_133728) do
+ActiveRecord::Schema.define(version: 2021_03_19_112158) do
 
   create_table "books", force: :cascade do |t|
     t.text "title"
