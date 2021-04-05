262852f1f99fffd319766a464d373e1af99f0526
mf-2021 <manamu.fujiwara@gmail.com>
2021-04-05 03:04:19 +0000

    [Add]asociating

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,14 @@ class User < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
+  # UseモデルはRelationshipクラスにfollowerを"follower_id"keyで持ってます
+  has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  # そして、followerを通して、followed_usersを持っています。ちなみにその際のkeyとなるカラムはfollowedです。（throughとsourceは"_id"が隠れている）
+  has_many :followed_users, through: :follower, source: :followed
+  #上記２コードの裏返し
+  has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :follower_users, through: :followed, source: :follower
+
 
   attachment :profile_image
 
